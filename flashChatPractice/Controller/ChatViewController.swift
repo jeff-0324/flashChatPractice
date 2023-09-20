8bee4ddba0413d8f4763124403a6b8ebff44df7b
jeff-0324 <jeffap324@gmail.com>
2023-09-20 23:13:17 +0900

    채팅창 만들기

--- flashChatPractice/Controller/ChatViewController.swift
+++ flashChatPractice/Controller/ChatViewController.swift
@@ -13,12 +13,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var message : [Message] = [
+    Message(sender: "abc1@abc.com", body: "hey!"),
+    Message(sender: "a1@abc.com", body: "Hi!"),
+    Message(sender: "abc1@abc.com", body: "What's up?"),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "⚡️FlashChat"
+        //tableView.delegate = self
+        tableView.dataSource = self
+        title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
     }
     
     @IBAction func sendButtonTapped(_ sender: UIButton) {
@@ -36,4 +45,23 @@ class ChatViewController: UIViewController {
         }
     }
      
+}
+
+extension ChatViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = message[indexPath.row].body
+        return cell
+    }
+    
+    
+}
+extension ChatViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
---
+++ flashChatPractice/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  flashChatPractice
+//
+//  Created by jae hoon lee on 2023/09/19.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String
+}
